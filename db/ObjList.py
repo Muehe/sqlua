f8622af4f8970fdec061aa12a7e0f7ed8975193f
BreakBB <b.boeing@online.de>
2024-03-22 18:43:51 +0100

    Remove cata code from base ObjList.py

--- db/ObjList.py
+++ db/ObjList.py
@@ -65,80 +65,50 @@ class ObjList():
         for a in cursor.fetchall():
             obj_tpl.append(a)
 
-        if self.version == 'cata':
-            print(" SELECT gameobject")
-            cursor.execute("SELECT id, map, position_x, position_y, guid FROM gameobject")
-            obj = {}
-            for a in cursor.fetchall():
-                if a[0] not in obj:
-                    obj[a[0]] = []
-                obj[a[0]].append(a)
-        else:
-            print('  SELECT gameobject_spawn_entry')
-            cursor.execute('SELECT * FROM gameobject_spawn_entry')
-            obj_spawn_entry = {}
-            for guid, entry in cursor.fetchall():
-                if guid not in obj_spawn_entry:
-                    obj_spawn_entry[guid] = []
-                obj_spawn_entry[guid].append(entry)
+        print('  SELECT gameobject_spawn_entry')
+        cursor.execute('SELECT * FROM gameobject_spawn_entry')
+        obj_spawn_entry = {}
+        for guid, entry in cursor.fetchall():
+            if guid not in obj_spawn_entry:
+                obj_spawn_entry[guid] = []
+            obj_spawn_entry[guid].append(entry)
 
-            print("  SELECT gameobject")
-            cursor.execute("SELECT id, map, position_x, position_y, guid FROM gameobject")
-            obj = {}
-            for a in cursor.fetchall():
-                if (a[0] == 0):
-                    if a[4] in obj_spawn_entry:
-                        for entry in obj_spawn_entry[a[4]]:
-                            if entry not in obj:
-                                obj[entry] = []
-                            obj[entry].append(a)
-                    #else:
-                        #print(f'Missing entry for GUID {a[4]}')
-                    continue
-                elif(a[0] not in obj):
-                    obj[a[0]] = []
-                obj[a[0]].append(a)
+        print("  SELECT gameobject")
+        cursor.execute("SELECT id, map, position_x, position_y, guid FROM gameobject")
+        obj = {}
+        for a in cursor.fetchall():
+            if (a[0] == 0):
+                if a[4] in obj_spawn_entry:
+                    for entry in obj_spawn_entry[a[4]]:
+                        if entry not in obj:
+                            obj[entry] = []
+                        obj[entry].append(a)
+                #else:
+                    #print(f'Missing entry for GUID {a[4]}')
+                continue
+            elif(a[0] not in obj):
+                obj[a[0]] = []
+            obj[a[0]].append(a)
 
-        if self.version == "cata":
-            print("  SELECT quest_relation")
-            obj_start = {}
-            obj_end = {}
-            # actor 0=creature, 1=gameobject
-            # entry=creature_template.entry or gameobject_template.entry
-            # quest=quest_template.entry
-            # role 0=start, 1=end
-            cursor.execute("SELECT entry, quest, role FROM quest_relations WHERE actor=1")
-            for a in cursor.fetchall():
-                entry = a[0]
-                quest = a[1]
-                if a[2] == 0:
-                    if quest not in obj_start:
-                        obj_start[quest] = []
-                    obj_start[quest].append((entry, quest))
-                elif a[2] == 1:
-                    if quest not in obj_end:
-                        obj_end[quest] = []
-                    obj_end[quest].append((entry, quest))
-        else:
-            print("  SELECT gameobject_questrelation")
-            cursor.execute("SELECT * FROM gameobject_questrelation")
-            obj_start = {}
-            for a in cursor.fetchall():
-                if(a[0] in obj_start):
-                    obj_start[a[0]].append(a)
-                else:
-                    obj_start[a[0]] = []
-                    obj_start[a[0]].append(a)
+        print("  SELECT gameobject_questrelation")
+        cursor.execute("SELECT * FROM gameobject_questrelation")
+        obj_start = {}
+        for a in cursor.fetchall():
+            if(a[0] in obj_start):
+                obj_start[a[0]].append(a)
+            else:
+                obj_start[a[0]] = []
+                obj_start[a[0]].append(a)
 
-            print("  SELECT gameobject_involvedrelation")
-            cursor.execute("SELECT * FROM gameobject_involvedrelation")
-            obj_end = {}
-            for a in cursor.fetchall():
-                if(a[0] in obj_end):
-                    obj_end[a[0]].append(a)
-                else:
-                    obj_end[a[0]] = []
-                    obj_end[a[0]].append(a)
+        print("  SELECT gameobject_involvedrelation")
+        cursor.execute("SELECT * FROM gameobject_involvedrelation")
+        obj_end = {}
+        for a in cursor.fetchall():
+            if(a[0] in obj_end):
+                obj_end[a[0]].append(a)
+            else:
+                obj_end[a[0]] = []
+                obj_end[a[0]].append(a)
 
         print("  SELECT locales_gameobject")
         cursor.execute("SELECT * FROM locales_gameobject")
