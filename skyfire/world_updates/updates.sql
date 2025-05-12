f957283fe9de0430353869f17a8b33d79bb46504
BreakBB <b.boeing@online.de>
2025-05-12 18:17:41 +0200

    Remove invalid debug texts from 31694

--- skyfire/world_updates/updates.sql
+++ skyfire/world_updates/updates.sql
@@ -3,3 +3,6 @@ UPDATE quest_template SET NextQuestId = 25127 WHERE Id = 25136;
 
 # There are gameobjects that have item=1, but mincountOrRef is set to a negative itemId. We set item to the absolute value of mincountOrRef
 UPDATE gameobject_loot_template SET item = ABS(mincountOrRef) WHERE item = 1 AND mincountOrRef < 0;
+
+# quest 31694 has some debug text in the objectives text and details which we remove
+UPDATE quest_template SET Objectives = '', Details = '' WHERE Id = 31694;
