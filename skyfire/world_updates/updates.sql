77326990c8d624efa6f50271e4a1f5ed57a43b41
BreakBB <b.boeing@online.de>
2025-05-12 18:00:24 +0200

    Update some invalid mincountOrRef entries in gameobject_loot_template

--- skyfire/world_updates/updates.sql
+++ skyfire/world_updates/updates.sql
@@ -1,1 +1,5 @@
+# quest 25136 has a NextQuestId which is not in the database
 UPDATE quest_template SET NextQuestId = 25127 WHERE Id = 25136;
+
+# There are gameobjects that have item=1, but mincountOrRef is set to a negative itemId. We set item to the absolute value of mincountOrRef
+UPDATE gameobject_loot_template SET item = ABS(mincountOrRef) WHERE item = 1 AND mincountOrRef < 0;
