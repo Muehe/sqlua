2d88da93481b60610b340013c4deebd360142d45
BreakBB <b.boeing@online.de>
2025-05-22 11:37:32 +0200

    Replace really high quest flags

--- trinity/updates.sql
+++ trinity/updates.sql
@@ -6,3 +6,9 @@ DELETE FROM `creature_questender` WHERE `id` > 80674;
 
 # quest 31694 has some debug text in the objectives text and details which we remove
 UPDATE quest_template SET LogDescription = '', QuestDescription = '' WHERE ID = 31694;
+
+# some quests have questflags set to a really high value
+# for odd numbers we set them to 1 to keep the repeatable flag
+# for even numbers we set them to 0
+UPDATE quest_template SET Flags = 1 WHERE Flags > 524296 AND MOD(Flags, 2) = 1; # odd
+UPDATE quest_template SET Flags = 0 WHERE Flags > 524296 AND MOD(Flags, 2) = 0; # even
