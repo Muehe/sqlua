3d42f54122f2bb9471b99895d9f791f6e6726887
BreakBB <b.boeing@online.de>
2025-05-26 20:02:31 +0200

    Set zoneId and areaId for some NPCs

--- trinity/updates.sql
+++ trinity/updates.sql
@@ -16,3 +16,103 @@ UPDATE quest_template_addon SET NextQuestId = 25064 WHERE Id = 31163;
 # for even numbers we set them to 0
 UPDATE quest_template SET Flags = 1 WHERE Flags > 524296 AND MOD(Flags, 2) = 1; # odd
 UPDATE quest_template SET Flags = 0 WHERE Flags > 524296 AND MOD(Flags, 2) = 0; # even
+
+# Stormwind
+UPDATE creature SET zoneId = 1519, areaId = 1519 WHERE id IN (
+    60931, 61809, 61837, 62106, 62419, 65048, 65066, 65068, 65051, 68868, 65069, 69334, 70296
+);
+
+# Darnassus
+UPDATE creature SET zoneId = 1657, areaId = 1657 WHERE id IN (
+    62450
+);
+
+# Silvermoon City
+UPDATE creature SET zoneId = 3487, areaId = 3487 WHERE id IN (
+    68085, 68086
+);
+
+# Orgrimmar
+UPDATE creature SET zoneId = 1637, areaId = 1637 WHERE id IN (
+    62445, 65008, 65058, 65060, 65061, 65063, 65065, 65071, 65072, 65074, 65076, 65078, 66022, 68869, 69333, 70301
+);
+
+# Northshire (Human starting area)
+UPDATE creature SET zoneId = 6170, areaId = 6170 WHERE id IN (
+    63258
+);
+
+# Coldridge Valley (Dwarf starting area)
+UPDATE creature SET zoneId = 6176, areaId = 6176 WHERE id IN (
+    63285
+);
+
+# New Tinkertown (Gnome starting area)
+UPDATE creature SET zoneId = 6457, areaId = 6457 WHERE id IN (
+    63238, 63239, 63241, 63242
+);
+
+# Deathknell (Undead starting area)
+UPDATE creature SET zoneId = 6454, areaId = 6454 WHERE id IN (
+    63272
+);
+
+# Sunstrider Isle (Blood Elf starting area)
+UPDATE creature SET zoneId = 6455, areaId = 6455 WHERE id IN (
+    63332
+);
+
+# Eversong Woods
+UPDATE creature SET zoneId = 3430, areaId = 3430 WHERE id IN (
+    62020
+);
+
+# Teldrassil
+UPDATE creature SET zoneId = 141, areaId = 141 WHERE id IN (
+    62242
+);
+
+# Ashenvale
+UPDATE creature SET zoneId = 331, areaId = 331 WHERE id IN (
+    62177
+);
+
+# Feralas
+UPDATE creature SET zoneId = 357, areaId = 357 WHERE id IN (
+    62395, 66352, 66361, 66363, 66364, 68805
+);
+
+# Un'Goro
+UPDATE creature SET zoneId = 490, areaId = 490 WHERE id IN (
+    62127, 62370, 62375
+);
+
+# Desolace
+UPDATE creature SET zoneId = 405, areaId = 405 WHERE id IN (
+    62184, 62185, 62186, 62187, 66372, 66375, 66376, 66377
+);
+
+# Silithus
+UPDATE creature SET zoneId = 1377, areaId = 1377 WHERE id IN (
+    61441
+);
+
+# Mount Hyjal
+UPDATE creature SET zoneId = 616, areaId = 616 WHERE id IN (
+    62178, 62373
+);
+
+# Deepholm
+UPDATE creature SET zoneId = 5042, areaId = 5042 WHERE id IN (
+    62181, 62182
+);
+
+# Uldum
+UPDATE creature SET zoneId = 5034, areaId = 5034 WHERE id IN (
+    62523
+);
+
+# Twilight Highlands
+UPDATE creature SET zoneId = 4922, areaId = 4922 WHERE id IN (
+    62904
+);
